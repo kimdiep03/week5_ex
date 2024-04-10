bc47599b71bc7cf6376ab4776da36ee0ae4825b8
kimdiep03 <162079945+kimdiep03@users.noreply.github.com>
2024-04-10 20:12:21 +1000

    update

--- public/script.js
+++ public/script.js
@@ -10,3 +10,7 @@ const ex_array = [ 'a', 'b', 'c' ]
 print (ex_array.length)
 
 ex_array.forEach (print)
+
+//const w2h = (_, i) => i * cnv.height / cnv.width
+//let values = Array (cnv.width).fill ().map (w2h)
+// -> height/ width = fraction ()
